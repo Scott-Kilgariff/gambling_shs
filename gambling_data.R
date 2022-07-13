b484b2874f11539e37d44bc073ddeeec5f833235
Scott-Kilgariff <scott.kilgariff@phs.scot>
2022-07-13 15:22:49 +0100

    Tidying up comments

--- gambling_data.R
+++ gambling_data.R
@@ -80,56 +80,70 @@ for (u in seq_along(urls)){
           `[[`("rowname") %>%
           as.numeric()
         
+        # the 2013 spreadsheet has a different layout
         if (year == 2013){
           blanks <- c(blanks)
         } else {
           blanks <- c(blanks,nrow(df)+1)
         }
         
-        
+        # the row to start at
         start <- 2
         
+        # the column headers
         col_headers <- df[2,][!is.na(df[2,])]
         
+        # a loop to move down each sheet and extract the tables in those sheets
         for (i in seq_along(blanks)){
-        
+          
+          #get the last row of each table
           end <- blanks[i]
           end <- end - 1
           
+          # slice table "i" and clean the column names
           df_slice <- df[start:end, ]
           colnames(df_slice) <- df_slice[1,]  
           df_slice <- df_slice[2:nrow(df_slice),]
           df_slice <- clean_names(df_slice)
           
+          # normalise the table
           df_slice_ages <- df_slice %>% 
             filter(!is.na(na_2)) %>%
             select(na_2:make_clean_names(col_headers[length(col_headers)])) %>%
             pivot_longer( cols = make_clean_names(col_headers), names_to = "category") %>%
             mutate(description = reported_by_text)
           
+          # creat a lookup for the weighted and unweighted category bases
           bases_lookup <- data.frame(t(df_slice)) %>% 
             add_rownames() %>% 
             select(c(1,5,6))
+          
+          # rename columns in lookup
           names(bases_lookup)[2] <- "Unweighted_bases"
           names(bases_lookup)[3] <- "Weighted_bases"
           
+          # pre 2014 the percentage figures were given as values between 0 and 1
           if (year<2015){
             mult <- 100
           } else {
             mult <- 1
           }
           
+          # left join the weighted and unweighted category bases
+          # and format the values
           df_slice_ages <- df_slice_ages %>%
             left_join(bases_lookup, c("category" = "rowname")) %>%
             mutate(value = ifelse(value=="-", 0, value)) %>%
             mutate(value = round(as.numeric(value) * mult, 2))
-        
+          
+          # create features called "measure" and "year" and assigne them with values
           df_slice_ages$measure <- measure
           df_slice_ages$year <- year
-          names(df_slice_ages)[names(df_slice_ages) == "na_2"] <- "response"
-          print(dim(df_slice_ages))
-          print(colnames(df_slice_ages))
           
+          # rename the "na_2" column as "response"
+          names(df_slice_ages)[names(df_slice_ages) == "na_2"] <- "response"
+          
+          #add a textual description based on sex code
           if (i == 1){
             df_slice_ages$sex <- "Male"
           } else if (i == 2){
@@ -138,15 +152,20 @@ for (u in seq_along(urls)){
             df_slice_ages$sex <- "All"
           }
           
+          # add an answer code description based on answer_code
           df_slice_ages$answer_code <- answer_code
           
+          # n==1 for the first iteration of the loop only
           if (n == 1){
             df_sheet <- df_slice_ages
           } else {
             df_sheet <- rbind(df_sheet, df_slice_ages)
           }
           
+          # set the "start" value for the next iteration round the loop
           start <- end + 3
+          
+          #increment n by 1
           n <- n + 1
       
         } # end of "i" loop
@@ -157,15 +176,20 @@ for (u in seq_along(urls)){
 
 df_data <- df_sheet
 
+#remove any characters after ", by " in the measure field
 df_data$measure <- sub(", by .*", "", as.character(df_data$measure))
 
+# remove any rows where the response == "All".  These always add to 100% and are not needed
 df_data <- df_data %>% filter(response != "All")
 
+# create a vector of strings to be replaced and 
+# a vector of their corresponding replacements
 cat_updates_1 <- c('quintile', 'semi_routine_occupations', 'x5th_least_deprived', 
                    'x4th', 'x3rd', 'x2nd', 'x1st_most_deprived')
 cat_updates_2 <- c('quintile', 'semi_routine_and_routine_occupations', 'least_deprived', 
                    'x4', 'x3', 'x2', 'most_deprived')
 
+# loop through each vector and its corresponding replacement
 for (i in seq_along(cat_updates_1)) {
 
   df_data$category <- sub(paste0(cat_updates_1[i],".*"), 
@@ -173,7 +197,12 @@ for (i in seq_along(cat_updates_1)) {
                           as.character(df_data$category))
 }
 
+# the measures being reported are not consistent from one year
+# to the next and therefore need to be mapped to a consistent set
 map_measure <- read.csv("map_measure.csv")
+
+# the answer codes being reported are not consistent from one year
+# to the next and therefore need to be mapped to a consistent set
 map_answer_code <- read.csv("map_answer_code.csv")
 
 df_data <- df_data %>%
@@ -182,64 +211,9 @@ df_data <- df_data %>%
 df_data <- df_data %>%
   left_join(map_answer_code, c("answer_code" = "answer_code"))
 
+# export the data as a csv
 write.csv(df_data, "df_sheet.csv")
 
-##########################################
-#          Plotting the data             #
-##########################################
-
-df_plot <- df_data %>% 
-  filter(sex == "Male" & response =="Yes" & description == "equivalised_income")
-
-df_plot$category <- factor(df_plot$category, levels=c("top_quintile","x4","x3", "x2", "bottom_quintile"))
-
-answer_code_map_values <- unique(df_plot$answer_code_map)
-
-for (a in seq_along(answer_code_map_values)){
-  print(paste0("\n\n",answer_code_map_values[a]))
-  df_plot_filter <- df_plot %>% filter(answer_code_map == answer_code_map_values[a] &
-                                         category %in% c("top_quintile","bottom_quintile"))
-  
-  measures <- unique(df_plot_filter$measure_map)
-  print(measures)
-  
-  for (m in seq_along(measures)){
-    plot <- df_plot_filter %>% filter(measure_map == measures[m])
-    print(dim(plot))
-    
-    p <- ggplot(plot, aes(x=year, y=value, group=category)) +
-      geom_line(aes(color=category))+
-      geom_point(aes(color=category)) +
-      labs(title=answer_code_map_values[a],
-           x="Year", 
-           y = "Percent answering Yes",
-           subtitle = measures[m])
-    print(p)
-  }
-}
-
-
-df_plot <- df_data %>% 
-  filter(sex == "Male" & response =="Yes" & description == "equivalised_income")
-
-df_plot$category <- factor(df_plot$category, levels=c("top_quintile","x4","x3", "x2", "bottom_quintile"))
-
-p <- ggplot(df_plot, aes(x=year, y=value, group=category)) +
-  geom_line(aes(color=category))+
-  geom_point(aes(color=category)) +
-  labs(x="Year", 
-       y = "Percent answering Yes") +
-  facet_wrap(~ measure_map)
-print(p)
-
-library(viridis)
-ggplot(df_plot, aes(year, category, fill=value)) +
-  geom_raster() +
-  scale_y_discrete(limits=rev, labels= c("75+", "65 to 74","55 to 64","45 to 54","35 to 44","25 to 34","16 to 24")) +
-  scale_fill_viridis(direction = -1, option = "B", begin = 0.5, end = 1) +
-  geom_hline(yintercept=c(1.5,2.5,3.5,4.5,5.5,6.5,7.5)) +
-  geom_text(aes(label = round(value, 1))) +
-  ylab("Age groups") +
-  theme_classic()
+# END
 
 
